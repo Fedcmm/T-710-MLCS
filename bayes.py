87cbc2e6e2e525f677316c5ad62fe14ee7edce42
Fedcmm <federico.cruciani@live.com>
2024-08-29 10:40:58 +0000

    - Add vocabulary size as parameter to get_most_frequent_words - Test the Bayes classifier for different dictionary sizes

--- bayes.py
+++ bayes.py
@@ -1,38 +1,31 @@
 import os
-import re
+import time
 
-import pandas as pd
+from pandas import DataFrame
 from matplotlib import pyplot as plt
 from seaborn import heatmap
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import precision_score, confusion_matrix
 from sklearn.naive_bayes import MultinomialNB
 
-from dictionary import get_most_frequent_words
+from vocabulary import get_most_frequent_words
 
+
+vocabulary_sizes = [100, 500, 1000, 2000, 3000]
 
 def get_labels(directory) -> list:
     labels = []
     for f_name in os.listdir(directory):
-        labels.append(1 if re.match('spmsg.{2,}.txt', f_name) else 0)
+        labels.append(1 if f_name.startswith("spmsg") else 0)
     return labels
 
-def read_files(directory) -> list:
-    files = []
-    for f_name in os.listdir(directory):
-        with open(f"{directory}/{f_name}", 'r') as f:
-            files.append(f.readlines()[2])
-
-    return files
-
-def create_data(directory) -> pd.DataFrame:
-    vocabulary = get_most_frequent_words('train-mails')["word"]
+def create_data(directory, vocabulary) -> DataFrame:
     vectorizer = CountVectorizer(input='filename', vocabulary=vocabulary)
 
     f_names = [f"{directory}/{f_name}" for f_name in os.listdir(directory)]
     count_vector = vectorizer.fit_transform(f_names)
 
-    return pd.DataFrame(
+    return DataFrame(
         data=count_vector.toarray(),
         columns=vectorizer.vocabulary,
         index=f_names
@@ -48,25 +41,30 @@ def show_confusion_matrix(y_test, y_pred):
     ax.yaxis.set_ticklabels(['False', 'True'])
     plt.show()
 
+def test_sizes():
+    for size in vocabulary_sizes:
+        print(f'\n\n===== Test with vocabulary size {size} =====')
+
+        ts = time.time()
+        vocabulary = get_most_frequent_words('train-mails', size)["word"]
+
+        x_train = create_data('train-mails', vocabulary)
+        y_train = get_labels('train-mails')
+        x_test = create_data('test-mails', vocabulary)
+        y_test = get_labels('test-mails')
+
+        bayes = MultinomialNB()
+        bayes.fit(x_train, y_train)
+        print(f'Elapsed time: {time.time() - ts}')
+
+        accuracy = bayes.score(x_test, y_test)
+        print(f"Accuracy: {accuracy}")
+
+        y_pred = bayes.predict(x_test)
+        print(f'Precision: {precision_score(y_test, y_pred)}')
+
 def main():
-    x_train = create_data('train-mails')
-    y_train = get_labels('train-mails')
-    x_test = create_data('test-mails')
-    y_test = get_labels('test-mails')
-
-    print(x_train.shape)
-    print(x_test.shape)
-
-    model = MultinomialNB()
-    model.fit(x_train, y_train)
-
-    accuracy = model.score(x_test, y_test)
-    print(f"Accuracy: {accuracy}")
-
-    y_pred = model.predict(x_test)
-    print(precision_score(y_test, y_pred))
-
-    show_confusion_matrix(y_test, y_pred)
+    test_sizes()
 
 if __name__ == '__main__':
     main()
--- vocabulary.py
+++ vocabulary.py
@@ -2,8 +2,9 @@ import os
 from pandas import DataFrame
 
 
-def extract_words(file_contents: str) -> list:
-    return filter(lambda s: s.isalpha(), file_contents.split(" "))
+def extract_words(file_contents: str) -> list[str]:
+    # "Subject:" is filtered out by isalpha()
+    return filter(lambda word: word.isalpha() and len(word) > 1, file_contents.split(" "))
 
 def count_words(words: list) -> dict[str, int]:
     count_dict = {}
@@ -20,33 +21,28 @@ def count_all_words(directory: str) -> dict[str, int]:
     total_count = {}
     for f_name in os.listdir(directory):
         with open(f"{directory}/{f_name}") as f:
-            words = extract_words(f.readlines()[2])
+            words = extract_words(f.read())
             count_dict = count_words(words)
             total_count = merge_dicts(total_count, count_dict)
     return total_count
 
-def get_most_frequent_words(directory: str) -> DataFrame:
+def get_most_frequent_words(directory: str, amount: int = 2000) -> DataFrame:
     """
-    Return the 2000 most frequent worlds that appear in the files contained in the
+    Return the most frequent words that appear in the files contained in the
     given directory
 
-    :param directory The directory to search files in
+    :param directory: The directory to search files in
+    :param amount: The amount of words to return
     """
     total_count = count_all_words(directory)
 
-    #return list(dict(sorted(total_count.items(), key=lambda x: x[1], reverse=True)).keys())[:2000]
-
     count_df = DataFrame(list(total_count.items()), columns=["word", "count"])
-    #count_df = pd.DataFrame.from_dict(total_count, orient='index', columns=['count'])
     count_df.sort_values(by=['count'], ascending=False, inplace=True)
     count_df.reset_index(drop=True, inplace=True)
-    return count_df.head(2000)
+    return count_df.head(amount)
 
 def main():
-    #print(get_most_frequent_words("train-mails"))
-    total_count = count_all_words('train-mails')
-    count_list = list(total_count.keys())
-    print(count_list)
+    print(get_most_frequent_words("train-mails"))
 
 if __name__ == '__main__':
     main()
